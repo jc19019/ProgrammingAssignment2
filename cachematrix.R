2888f4eed31de6544b56ebd5fc8111aa8f6aa0f1
Jim Campbell <jcampbell@ubalt.edu>
2015-10-22 11:35:55 -0400

    final commit with proper comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,40 @@
 ##Programming Assignment #2 - R Programming
 ##Authored by: Jim Campbell
 
-## The makeCacheMatrix function creates a special "matrix" object
+##This program will do the following:  Write the following functions:
+  
+##  1.  `makeCacheMatrix`: This function creates a special "matrix" object
 ##that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+##  2.  `cacheSolve`: This function computes the inverse of the special
+##"matrix" returned by `makeCacheMatrix` above. If the inverse has
+##already been calculated (and the matrix has not changed), then
+##`cacheSolve` should retrieve the inverse from the cache.
 
+##Computing the inverse of a square matrix can be done with the `solve`
+##function in R. For example, if `X` is a square invertible matrix, then
+##`solve(X)` returns its inverse.
+
+##For this assignment, assume that the matrix supplied is always
+##invertible.
+##--------------------end of header----------------------------------------##
+
+
+## The makeCacheMatrix function creates a special "matrix" object
+##that can cache its inverse.  The function simply returns a matrix based on a passed argument
+
+makeCacheMatrix <- function(x = matrix()) {
+        NewCacheMatrix <- makeCacheMatrix(matrix(nrow = 2, ncol = 2))
 }
 
 
-## The cacheSolve function retrieves the inverse from the cache.
+## The cacheSolve function retrieves the inverse from the cache.  This function
+##uses the solve() function to create the inverse value from an originating matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        InverseCacheSolve <- solve(makeCacheMatrix)
+        if(InverseCacheSolve == makeCacheMatrix){
+                return InverseCacheSolve
+        }
+        ReturnMatrix <- makeCacheMatrix
 }
